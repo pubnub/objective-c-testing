19630b79af56670548ba691fe77ca54fcfac2b19
Jordan Zucker <jordan.zucker@gmail.com>
2016-07-15 15:54:46 -0700

    Fleshed out simple subscribe loop test case and test target test

---
+++ Tests/Tests/TestCases/SubscribeTestCase.m
@@ -0,0 +1,27 @@
+//
+//  SubscribeTestCase.m
+//  Tests
+//
+//  Created by Jordan Zucker on 7/15/16.
+//  Copyright © 2016 Jordan Zucker. All rights reserved.
+//
+
+#import <PubNubTesting/PubNubTesting.h>
+
+@interface SubscribeTestCase : PNTSubscribeLoopTestCase
+
+@end
+
+@implementation SubscribeTestCase
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+@end
